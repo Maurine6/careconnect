bccba05294e96dd822e45d66d6b0d6049b7ce40b
rogers.machara <rogersmachara13@gmail.com>
2024-07-11 11:57:45 +0300

    setup structure and auth

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -11,6 +11,8 @@ function Layout({ children }) {
           <li><Link to="/patients">Patients</Link></li>
           <li><Link to="/appointments">Appointments</Link></li>
           <li><Link to="/staff">Staff</Link></li>
+          <li><Link to="/login">Login</Link></li> 
+          <li><Link to="/signup">Signup</Link></li>
         </ul>
       </nav>
       <main>{children}</main>
