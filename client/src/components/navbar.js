c59c4d988a14758c25aad2bc964d8023c549818d
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-07-15 17:43:39 +0300

    added some styling

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,26 +3,26 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-<nav class="navbar navbar-expand-sm bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="/">Careconnect</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-sm bg-light">
+  <div className="container">
+    <a className="navbar-brand" href="/">Careconnect</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto"> <li class="nav-item">
-          <Link to="/" class="nav-link active">Home</Link> </li>
-        <li class="nav-item">
-          <Link to="/services_offered" class="nav-link">Services Offered</Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto"> <li className="nav-item">
+          <Link to="/" className="nav-link active">Home</Link> </li>
+        <li className="nav-item">
+          <Link to="/services_offered" className="nav-link">Services Offered</Link>
         </li>
-        <li class="nav-item">
-          <Link to="/login" class="nav-link">Login</Link>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">Login</Link>
         </li>
-        <li class="nav-item">
-          <Link to="/signup" class="nav-link">Signup</Link>
+        <li className="nav-item">
+          <Link to="/signup" className="nav-link">Signup</Link>
         </li>
-        <li class="nav-item">
-          <Link to="/about-us" class="nav-link">About Us</Link>
+        <li className="nav-item">
+          <Link to="/about-us" className="nav-link">About Us</Link>
         </li>
       </ul>
     </div>
